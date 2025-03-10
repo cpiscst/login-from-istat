771d6ada6dd2c085930664287484437d794fff15
cpi <cpicstlab20@gmail.com>
2025-03-10 12:05:12 +0600

    login from update

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="flex justify-center">
+      <div>
+      <div className='bg-white'>
+    <p className='text-xl front-bold'> User Name or Email:</p>
+    <input type="text" className='border-2 rounded-md h-[40px] w-[300px] font-blod text-gray-500 text-xl p-3' />
+    </div>
+    <div className='bg-white'>
+    <p className='text-xl front-bold'> Password:</p>
+    <input type="text" className='border-2 rounded-md h-[40px] w-[300px] font-blod  text-gray-500 text-xl p-3' />
+    </div> 
+
+      </div>
+    </div>
+    
+    
+  
+     
+    </>
+  )
+}
+
+export default App
