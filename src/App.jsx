ca11b6b94de0085a4b40a5174c089460fc779a47
Israt <cpicstlab20@gamil.com>
2025-03-11 11:04:44 +0600

    'update'

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,31 @@ function App() {
 
   return (
     <>
-    <div className="flex justify-center">
-      <div>
+    <div className="flex justify-center ">
+      <div className="my-20 max-w-[600px] pl-5 border-2 p-5 ">
+      <h1 className="text-6xl font-bold pb-10">
+  Login-from
+</h1>
       <div className='bg-white'>
     <p className='text-xl front-bold'> User Name or Email:</p>
     <input type="text" className='border-2 rounded-md h-[40px] w-[300px] font-blod text-gray-500 text-xl p-3' />
     </div>
+    
     <div className='bg-white'>
     <p className='text-xl front-bold'> Password:</p>
     <input type="text" className='border-2 rounded-md h-[40px] w-[300px] font-blod  text-gray-500 text-xl p-3' />
-    </div> 
-
+    </div>
+    <div className="my-3"></div> 
+    <div className='bg-white'>
+    <p className='text-right front-bold'> Forget Password?</p>
+   
+    </div>
+    <div className="flex justify-center">
+    <button className="bg-blue-300 p-3 text-black border-2 rounded-md w-[150px] h-[50px] m-15">
+    Log-in
+  </button>
+      </div> 
+   
       </div>
     </div>
     
